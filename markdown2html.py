16873c77c0136e0f4ebb688094b7a2b38cf15cfa
abdisag1 <myabdisa@gmail.com>
2022-07-03 21:35:00 +0300

    modified task 0

--- markdown2html.py
+++ markdown2html.py
@@ -2,13 +2,12 @@
 import sys
 import os
 
-
+markdown = sys.argv[1]
 if(len(sys.argv) < 3):
     print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
     exit (1)
-if(sys.argv[1] == "README.md"):    
-    markdown = sys.argv[1]
-    markdownfile = os.path.exists(markdown)
-    if(markdownfile == False):
-        print("missing README.md")
-        exit (1)
+   
+markdownfile = os.path.exists(markdown)
+if(markdownfile == False):
+    print("missing {markdown}", file=sys.stderr)
+    exit (1)
