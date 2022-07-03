85b6762ad3233676d5d15536d421b65e58d6ea9e
abdisag1 <myabdisa@gmail.com>
2022-07-03 21:52:50 +0300

    removed file stterr

--- markdown2html.py
+++ markdown2html.py
@@ -6,11 +6,12 @@ import sys
 import os
 
 if __name__ == '__main__':
+    """ this function takes two arguments markdown and htmlfile"""
     if(len(sys.argv) < 3):
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     markdown = sys.argv[1]
     markdownfile = os.path.exists(markdown)
     if(markdownfile == False):
-        print(f'missing {markdown}', file=sys.stderr)
+        print(f'missing {markdown}')
         exit(1)
