a295d4e3eb1c586e4411cb92b948400ee36bd4fb
abdisag1 <myabdisa@gmail.com>
2022-07-03 21:45:15 +0300

    added main fuction

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""
+takes two argumnets
+"""
 import sys
 import os
 
-markdown = sys.argv[1]
-if(len(sys.argv) < 3):
-    print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
-    exit (1)
-   
-markdownfile = os.path.exists(markdown)
-if(markdownfile == False):
-    print("missing {markdown}", file=sys.stderr)
-    exit (1)
+if __name__ == '__main__':
+    if(len(sys.argv) < 3):
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    markdown = sys.argv[1]
+    markdownfile = os.path.exists(markdown)
+    if(markdownfile == False):
+        print(f'missing {markdown}', file=sys.stderr)
+        exit(1)
