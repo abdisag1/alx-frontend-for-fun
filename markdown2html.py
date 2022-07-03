662b5ad6fb493cf3c45e4edd24c79d1aa91d2d15
abdisag1 <myabdisa@gmail.com>
2022-07-03 22:55:31 +0300

    done task 1

--- markdown2html.py
+++ markdown2html.py
@@ -4,14 +4,24 @@ takes two argumnets
 """
 import sys
 import os
+import markdown
 
 if __name__ == '__main__':
     """ this function takes two arguments markdown and htmlfile"""
     if(len(sys.argv) < 3):
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-    markdown = sys.argv[1]
-    markdownfile = os.path.exists(markdown)
+    markdownfile = sys.argv[1]
+    htmlfile = sys.argv[2]
+    markdownfile = os.path.exists(markdownfile)
     if(markdownfile == False):
-        print(f'missing {markdown}',file=sys.stderr)
+        print(f'missing {markdownfile}',file=sys.stderr)
         exit(1)
+    
+    
+    with open(sys.argv[1], 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+
+    with open(sys.argv[2], 'w') as f:
+        f.write(html)
