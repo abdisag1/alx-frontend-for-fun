0eaba115e98e04ea7bcee7195f1171f6f0fec5d1
abdisag1 <myabdisa@gmail.com>
2022-07-03 23:24:27 +0300

    added condition for task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python3
-"""
-takes two argumnets
-"""
+'''
+A script that codes markdown to HTML
+'''
 import sys
 import os
-import markdown
+import re
 
 if __name__ == '__main__':
-    """ this function takes two arguments markdown and htmlfile"""
-    if(len(sys.argv) < 3):
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        exit(1)
-    markdownfile = sys.argv[1]
-    htmlfile = sys.argv[2]
-    markdownfile = os.path.exists(markdownfile) and os.path.isfile(markdownfile)
-    if(markdownfile == False):
-        print(f'missing {markdownfile}', file=sys.stderr)
-        exit(1)
 
-    with open(sys.argv[1], 'r', encoding='utf-8') as f:
-        text = f.read()
-        html = markdown.markdown(text)
+    # Test that the number of arguments passed is 2
+    if len(sys.argv[1:]) != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
 
-    with open(sys.argv[2], 'w', encoding='utf-8') as f:
-        f.write(html)
+    # Store the arguments into variables
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Checks that the markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
+
+    with open(input_file, encoding='utf-8') as file_1:
+        html_content = []
+        md_content = [line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r'#{1,6} ', line)
+            if len(heading) > 1:
+                # Compute the number of the # present to
+                # determine heading level
+                h_level = len(line[:line.find(heading[1])-1])
+                # Append the html equivalent of the heading
+                html_content.append(
+                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
+                )
+            else:
+                html_content.append(line)
+
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
